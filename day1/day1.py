0dca64edcb7e86c3d81458be2b4ea28085fa85fb
Elanor Stark <elanor99stark@gmail.com>
2023-12-01 06:35:32 +0000

    Day 1 read in change

--- day1/day1.py
+++ day1/day1.py
@@ -1,8 +1,8 @@
 import re
 
 
-def read_in_1d():
-    with open("day1.txt", "r") as input_file:
+def read_in():
+    with open("input.txt", "r") as input_file:
         lines = [line.strip() for line in input_file]
     return lines
 
@@ -58,7 +58,7 @@ def p2(values):
 
 
 def main():
-    values = read_in_1d()
+    values = read_in()
     # print(values)
     print(p1(values))
 
