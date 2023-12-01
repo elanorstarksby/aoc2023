ea3d161be8aac888a5b78d3c72c2ded8e0079242
Elanor Stark <elanor99stark@gmail.com>
2023-12-01 06:31:50 +0000

    Day 1 cleanup

--- day1.py
+++ day1.py
@@ -3,74 +3,66 @@ import re
 
 def read_in_1d():
     with open("day1.txt", "r") as input_file:
-        lines = [[l for l in line.strip()] for line in input_file]
+        lines = [line.strip() for line in input_file]
     return lines
 
 
-def calculate_total(line):
+def calculate_total(line, part):
     # print(line)
-    digits = {"one": 1, "two": 2, "three": 3, "four": 4, "five": 5, "six": 6, "seven": 7, "eight": 8, "nine": 9}
-    first_digit = len(line)
-    first_digit_value = 0
-    last_digit = -1
-    last_digit_value = 0
-    # word digits
-    for digit in digits.keys():
-        locations = [m.start() for m in re.finditer(re.escape(str(digit)), "".join(line))]
-        # print(locations)
-        if locations:
-            if min(locations) < first_digit:
-                first_digit = min(locations)
-                first_digit_value = digits[digit]
-            if max(locations) > last_digit:
-                last_digit = max(locations)
-                last_digit_value = digits[digit]
+    digits = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    first = [len(line), 0]
+    last = [-1, 0]
+    for digit_i in range(1, 10):
+        # word digits
+        if part == 2:
+            digit = digits[digit_i - 1]
+            locations_letters = [m.start() for m in re.finditer(re.escape(str(digit)), line)]
+            # print(locations)
+            if locations_letters:
+                if min(locations_letters) < first[0]:
+                    first[0] = min(locations_letters)
+                    first[1] = digit_i
+                if max(locations_letters) > last[0]:
+                    last[0] = max(locations_letters)
+                    last[1] = digit_i
+
+        # number digits
+        locations_numbers = [m.start() for m in re.finditer(re.escape(str(digit_i)), line)]
+        if locations_numbers:
+            if min(locations_numbers) < first[0]:
+                first[0] = min(locations_numbers)
+                first[1] = digit_i
+            if max(locations_numbers) > last[0]:
+                last[0] = max(locations_numbers)
+                last[1] = digit_i
+
     # print(first_digit_value, last_digit_value)
 
-    # number digits
-    for i in range(0, 10):
-        locations = [m.start() for m in re.finditer(str(i), "".join(line))]
-        if locations:
-            if min(locations) < first_digit:
-                first_digit = min(locations)
-                first_digit_value = i
-            if max(locations) > last_digit:
-                last_digit = max(locations)
-                last_digit_value = i
     # print(first_digit_value, last_digit_value)
     # print(10 * first_digit_value + last_digit_value)
-    return 10 * first_digit_value + last_digit_value
+    return 10 * first[1] + last[1]
+
+
+def p1(values):
+    p1_total = 0
+    for value in values:
+        p1_total += calculate_total(value, 1)
+    return p1_total
+
+
+def p2(values):
+    p2_total = 0
+    for value in values:
+        p2_total += calculate_total(value, 2)
+    return p2_total
 
 
 def main():
     values = read_in_1d()
     # print(values)
-    total = 0
-    for value in values:
-        digits_added = 0
-        final = 0
-        for c in value:
+    print(p1(values))
 
-            try:
-                v = int(c)
-                if digits_added == 0:
-                    # print(10 * v)
-                    total += 10 * v
-                    final = v
-                else:
-                    final = v
-                digits_added += 1
-            except ValueError:
-                # print("NO", c)
-                pass
-        # print(final)
-        total += final
-    print(total)
-
-    p2_total = 0
-    for value in values:
-        p2_total += calculate_total(value)
-    print(p2_total)
+    print(p2(values))
 
 
 if __name__ == '__main__':
