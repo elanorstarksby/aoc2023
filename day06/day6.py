407e1a37960bcabc6665507a2179508bb7e2c1de
Elanor Stark <elanor99stark@gmail.com>
2023-12-07 17:48:12 +0000

    Day 6: mathematical solution

--- day06/day6.py
+++ day06/day6.py
@@ -1,3 +1,6 @@
+import math
+
+
 def read_in():
     with open("input.txt", "r") as input_file:
         lines = [line.strip().split(":")[1].split() for line in input_file]
@@ -19,8 +22,29 @@ def p1(values):
     return ways_product
 
 
+def search_start(time, record, start, end):
+    mp = start + (end - start) // 2
+    if distance_gone(time, mp) > record:
+        if distance_gone(time, mp - 1) <= record:
+            return mp
+        return search_start(time, record, start, mp)
+    else:  # distance <= record
+        if distance_gone(time, mp + 1) > record:
+            return mp
+        return search_start(time, record, mp, end)
+
+
 def p2(values):
-    return p1([[int("".join(r))] for r in values])
+    [time, distance] = [int("".join(r)) for r in values]
+    D = time  # length of race
+    R = distance  # record
+    t_1 = (D + math.sqrt(D**2 - (4 * R))) / (-2)
+    if -2 * t_1 - D == 0:  # if t_1 is a turning point, there are no solutions
+        return 0
+    t_2 = (D - math.sqrt(D**2 - (4 * R))) / (-2)
+    s = math.floor(min(t_1, t_2)) + 1
+    e = math.ceil(max(t_1, t_2))
+    return e - s
 
 
 def main():
